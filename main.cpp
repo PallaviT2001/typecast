c1cf1268c220cbe91eb727a465ffc4c048bf2ae8
PallaviT2001 <165918298+PallaviT2001@users.noreply.github.com>
2024-10-14 12:26:29 +0530

    first commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    float n=0;
+    float eggsindozens;
+    cout<<"enter number of eggs"<<endl;
+    cin>>n;
+    eggsindozens=n/12;
+    cout<<eggsindozens<<" dozens eggs are there"<<endl;
+}
